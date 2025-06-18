206545da0a2a64947f03f8f50e9282b66b2c5b2e
2021147542 <101931965+2021147542@users.noreply.github.com>
2025-06-18 18:38:11 +0900

    Revert "Merge branch 'shindongjoo' of https://github.com/3DAngryBird/3DAngryBird into shindongjoo"
    This reverts commit b002e6003efb1076c7f8a20657cecaecab2ae76d, reversing
    changes made to b5efd1eff34b012366140dc46f0b7cfc5c75a42f.

--- main.js
+++ main.js
@@ -349,6 +349,7 @@ function initStage(stageNumber) {
     animFp = "./models/buildings/animations/HouseAnime.glb"; // 예시 파일명
   } else {
     animFp = "./models/buildings/animations/DominoAnime.glb";
+  }
 
   loader.load(animFp, (gltf) => {
     console.log(`Animation for stage ${stageNumber} loaded from ${animFp}`);
@@ -456,7 +457,6 @@ function initStage(stageNumber) {
     spawnCharacter(kingpigpath, new THREE.Vector3(0, 0, -17.8), 2);
 
   }
-
 
 
   // spawnCharacter(pigpath, new THREE.Vector3(-0.18, 0, -7.52), 2);
@@ -832,9 +832,7 @@ function spawnCharacter(name, position, scale) {
   return mesh;
 }
 
-
 const DEATH_THRESHOLD = [3.5, 5, 8];
-
 
 // =======================================
 //  PointerLockControls 세팅
